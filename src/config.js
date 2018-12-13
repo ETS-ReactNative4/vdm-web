ae12da46f3622825c9711559d8ae80dd66b436b9
nathannoble <nathan.noble@gmail.com>
2018-12-13 11:35:01 -0500

    Update gitignore

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
 export const VDM_SERVICE_HOST = 'http://52.201.45.52:9988/vdm';
 export const VDM_UPLOAD_HOST = 'http://localhost:4000/api/vdm';
+export const VDM_META_SERVICE_HOST = 'http://52.201.45.52:8080/v1';
+
+// LOCAL OVERRIDE
 export const VDM_SERVICE_HOST_LOCAL = 'http://localhost:4000/api/vdm';
-
-// LOCAL
-//export const VDM_SERVICE_HOST = 'http://localhost:4000/api/vdm';
-
-export const VDM_META_SERVICE_HOST = 'http://52.45.141.104:8080/vdm/v1';
+// export const VDM_SERVICE_HOST = 'http://localhost:4000/api/vdm';
 // export const VDM_META_SERVICE_HOST = 'http://localhost:4000/api/vdm';
-export const VDM_META_SERVICE_HOST_LOCAL = 'http://localhost:4000/api/vdm';
+
+
+
